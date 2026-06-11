7fd103716597ab5d39f0a77f4ddcec28f3e2a013
Házi Nándor <hazi.nandor21@gmail.com>
2026-06-11 17:18:53 +0200

    disable vercel image optimization, use supabase transforms only

--- next.config.js
+++ next.config.js
@@ -2,16 +2,7 @@
 const nextConfig = {
   serverExternalPackages: ['@supabase/supabase-js'],
   images: {
-    minimumCacheTTL: 2592000,
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'gbauulozffwjpqkgrmla.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
